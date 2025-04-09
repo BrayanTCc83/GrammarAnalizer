7c666894e73b5a250124192fd1cbfea8047a36f0
Brayan <btellez948@gmail.com>
2025-04-09 00:22:52 -0600

    [Fix] Update test

--- test/main.c
+++ test/main.c
@@ -1,7 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "../src/headers/global.h"
+#include "../src/headers/linkedlist.h"
 #include "../src/headers/set.h"
+#include "../src/headers/hashtable.h"
 
 ComparationResult int_compare(GenericValue, GenericValue);
 char *int_to_string(GenericValue);
@@ -9,48 +11,19 @@ int *new_int(int);
 void delete_int(GenericValue);
 
 ComparationResult char_compare(GenericValue, GenericValue);
+int char_to_hash(GenericValue);
 char *char_to_string(GenericValue);
 char *new_char(char);
 
 int main(int argc, char *argv[]) {
-    Set *set1 = new_set(int_compare, delete_int, int_to_string);
-    Set *set2 = new_set(int_compare, delete_int, int_to_string);
+    HashTable *table = new_hash_table(30);
+    hash_table_set_key_fn(table, char_compare, NULL, char_to_string, char_to_hash);
+    hash_table_set_value_fn(table, int_compare, delete_int, int_to_string);
 
-    set_insert(set1, new_int(350));
-    set_insert(set1, new_int(200));
-    set_insert(set1, new_int(400));
-    set_insert(set1, new_int(100));
-    set_insert(set1, new_int(300));
-    set_insert(set1, new_int(160));
-
-    set_insert(set2, new_int(350));
-    set_insert(set2, new_int(200));
-    set_insert(set2, new_int(870));
-    set_insert(set2, new_int(1500));
-    set_insert(set2, new_int(80));
-    set_insert(set2, new_int(70));
-    set_insert(set2, new_int(45));
-
-    Set *set3 = set_union(*set1, *set2);
-    Set *set4 = set_difference(*set1, *set2);
-    Set *set5 = set_difference(*set2, *set1);
-    printf("Set1: %s with size %i.\n", set_to_string(*set1), set1->size);
-    printf("Set2: %s with size %i.\n", set_to_string(*set2), set2->size);
-    printf("Set1 U Set2: %s with size %i.\n", set_to_string(*set3), set3->size);
-    printf("Set1 - Set2: %s with size %i.\n", set_to_string(*set4), set4->size);
-    printf("Set2 - Set1: %s with size %i.\n", set_to_string(*set5), set5->size);
-
-    printf("Looking for 1000, into set '%s'.\n", set_search(*set1, new_int(1000)) == true ? "EXISTS": "NOT EXISTS");
-    printf("Set as list of integers: %s\n", linked_list_to_string(*set_as_list(*set1)));
-    set_remove(set1, new_int(1000));
-    printf("Set of integers: %s\n", set_to_string(*set1));
-    printf("Looking for 1000, into set '%s'.\n", set_search(*set1, new_int(1000)) == true ? "EXISTS": "NOT EXISTS");
-
-    delete_set(set1);
-    delete_set(set2);
-    //delete_set(set3);
-    //delete_set(set4);
-    //delete_set(set5);
+    hash_table_insert(table, new_char('A'), new_int(10));
+    hash_table_insert(table, new_char('A'), new_int(15));
+    hash_table_insert(table, new_char('B'), new_int(10));
+    hash_table_insert(table, new_char('C'), new_int(10));
 
     return EXIT_SUCCESS;
 }
@@ -94,6 +67,10 @@ char *char_to_string(GenericValue c) {
     char *string = to_string();
     sprintf(string, "%c", *((char*)c));
     return string;
+}
+
+int char_to_hash(GenericValue c) {
+    return *((char*)c);
 }
 
 char *new_char(char c) {
