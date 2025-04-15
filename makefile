f22f1a237074bf64382465c4ac319d2dc09d06f2
Brayan <btellez948@gmail.com>
2025-04-15 17:59:01 -0600

    [Fix] Updating gitignore, makefile and main program.

--- makefile
+++ makefile
@@ -7,6 +7,8 @@ TEST=test
 TEST_MAIN=$(TEST)/main.c
 GLOBAL=$(SOURCE)/global.c
 STRUCTS=$(SOURCE)/structs
+INTERPRETER=$(SOURCE)/interpreter/*.c
+GRAMMAR=$(SOURCE)/grammar/*.c
 SET=$(STRUCTS)/set/*.c
 LIST=$(STRUCTS)/list/*.c
 HASHTABLE=$(STRUCTS)/hashtable/*.c
@@ -16,13 +18,13 @@ OUTPUT=$(DISTRIBUTION)/GrammarEvaluator
 
 CC:=gcc
 
-compile: $(MAIN) $(GLOBAL) $(SET) $(LIST) $(HASHTABLE) $(HEADERS)/*.h | $(DISTRIBUTION)
+compile: $(MAIN) $(GLOBAL) $(SET) $(LIST) $(HASHTABLE) $(INTERPRETER) $(GRAMMAR) $(HEADERS)/*.h | $(DISTRIBUTION)
 	@echo Compile program.
-	$(CC) -o $(OUTPUT) $(MAIN) $(GLOBAL) $(SET) $(LIST) $(HASHTABLE) -I $(HEADERS) 
+	$(CC) -o $(OUTPUT) $(MAIN) $(GLOBAL) $(SET) $(LIST) $(HASHTABLE) $(INTERPRETER) $(GRAMMAR) -I $(HEADERS) 
 
-test: $(TEST_MAIN) $(GLOBAL) $(SET) $(LIST) $(HASHTABLE) $(HEADERS)/*.h | $(DISTRIBUTION)
+test: $(TEST_MAIN) $(GLOBAL) $(SET) $(LIST) $(HASHTABLE) $(INTERPRETER) $(GRAMMAR) $(HEADERS)/*.h | $(DISTRIBUTION)
 	@echo Compile program.
-	$(CC) -o $(OUTPUT) $(TEST_MAIN) $(GLOBAL) $(SET) $(LIST) $(HASHTABLE) -I $(HEADERS)  -DDEV
+	$(CC) -o $(OUTPUT) $(TEST_MAIN) $(GLOBAL) $(SET) $(LIST) $(HASHTABLE) $(INTERPRETER) $(GRAMMAR) -I $(HEADERS) -DDEV
 
 $(DISTRIBUTION):
 	@echo Making directory $(DISTRIBUTION).
--- test/main.c
+++ test/main.c
@@ -4,77 +4,42 @@
 #include "../src/headers/linkedlist.h"
 #include "../src/headers/set.h"
 #include "../src/headers/hashtable.h"
-
-ComparationResult int_compare(GenericValue, GenericValue);
-char *int_to_string(GenericValue);
-int *new_int(int);
-void delete_int(GenericValue);
-
-ComparationResult char_compare(GenericValue, GenericValue);
-int char_to_hash(GenericValue);
-char *char_to_string(GenericValue);
-char *new_char(char);
+#include "../src/headers/interpreter.h"
 
 int main(int argc, char *argv[]) {
-    HashTable *table = new_hash_table(30);
-    hash_table_set_key_fn(table, char_compare, NULL, char_to_string, char_to_hash);
-    hash_table_set_value_fn(table, int_compare, delete_int, int_to_string);
+    if(argc < 1) {
+        fprintf(stderr, "You must give almost one grammar file to test.");
+        exit(EXIT_FAILURE);
+    }
 
-    hash_table_insert(table, new_char('A'), new_int(10));
-    hash_table_insert(table, new_char('A'), new_int(15));
-    hash_table_insert(table, new_char('B'), new_int(10));
-    hash_table_insert(table, new_char('C'), new_int(10));
+    for(int i = 1; i <argc; i++) {
+        FILE *file = fopen(argv[i], "r");
+        Scanner *scanner = new_scanner(file);
+        Parser *parser = new_parser(scanner);
+        Interpreter *interpreter = new_interpreter(parser);
+        interpreter_start(interpreter);
+    
+        Grammar *grammar = interpreter->grammar;
+    
+        printf("No terminals: %s\n", set_to_string(*grammar->no_terminal));
+        printf("Terminals: %s\n", set_to_string(*grammar->terminal));
+        printf("Start: %s\n", set_to_string(*grammar->start));
+        printf("Productions: %s\n", set_to_string(*grammar->productions));
+        printf("Anulables: %s\n", set_to_string(*grammar->anulable));
+        printf("First sets: \n");
+        grammar_show_first_sets(*grammar);
+        printf("Follow sets: \n");
+        grammar_show_follow_sets(*grammar);
+        printf("Selection sets: \n");
+        grammar_show_selection_sets(*grammar);
+        printf("The grammar is: %s\n",
+            grammar->type == GRAMMAR_S ? "Grammar S" : grammar->type == GRAMMAR_Q ? "Grammar q" : grammar->type == GRAMMAR_LL_1 ? "Grammar LL(1)" : "Grammar not recognized"
+        );
+
+        //delete_interpreter(interpreter);
+        //delete_grammar(grammar);
+        //fclose(file);
+    }
 
     return EXIT_SUCCESS;
-}
-
-int *new_int(int n) {
-    instance(int);
-    *o = n;
-    return o;
-}
-
-void delete_int(GenericValue n) {
-    *(int*)n = 0;
-    free((int*)n);
-}
-
-char *int_to_string(GenericValue n) {
-    char *string = to_string();
-    sprintf(string, "%i", *((int*)n));
-    return string;
-}
-
-ComparationResult int_compare(GenericValue n1, GenericValue n2) {
-    int N1 = *(int*)n1, N2 = *(int*)n2;
-    ComparationResult result = N1 < N2 ? SMALLER : N1 > N2 ? BIGGER : EQUALS;
-    #ifdef DEV
-    LOG("Comparing values %i and %i, result %i.", N1, N2, result);
-    #endif
-    return result;
-}
-
-ComparationResult char_compare(GenericValue c1, GenericValue c2) {
-    char C1 = *(char*)c1, C2 = *(char*)c2;
-    ComparationResult result = C1 < C2 ? SMALLER : C1 > C2 ? BIGGER : EQUALS;
-    #ifdef DEV
-    LOG("Comparing values %c and %c, result %i.", C1, C2, result);
-    #endif
-    return result;
-}
-
-char *char_to_string(GenericValue c) {
-    char *string = to_string();
-    sprintf(string, "%c", *((char*)c));
-    return string;
-}
-
-int char_to_hash(GenericValue c) {
-    return *((char*)c);
-}
-
-char *new_char(char c) {
-    instance(char);
-    *o = c;
-    return o;
 }
